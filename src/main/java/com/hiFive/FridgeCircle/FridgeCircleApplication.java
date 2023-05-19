798ea8acb7180726151eaf3883e2a66352d03d5b
Sabīne Liepiņa <sabine.e.liepina@gmail.com>
2023-05-19 21:03:40 +0300

    first commit

---
+++ src/main/java/com/hiFive/FridgeCircle/FridgeCircleApplication.java
@@ -0,0 +1,13 @@
+package com.hiFive.FridgeCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeCircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiFive/FridgeCircle/FridgeCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiFive.FridgeCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
